f0121daa74796c7a7b5d60132da617fd6f5c57b0
Devin Riley <devin.riley144@gmail.com>
2023-01-31 12:06:39 -0600

    finished exercise

--- OpenWeatherExercise/Program.cs
+++ OpenWeatherExercise/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace OpenWeatherExercise
+﻿using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace OpenWeatherExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            string zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?units=imperial&zip={zipCode}&appid={APIKey}";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
         }
     }
 }
---
+++ OpenWeatherExercise/WeatherMap.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherExercise
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+            
+            //Return and store JSON response from API
+            var response = client.GetStringAsync(apiCall).Result;
+            
+            //use Newtonsoft.JSON to parse JSON RESPONSE
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+        }
+    }
+}
